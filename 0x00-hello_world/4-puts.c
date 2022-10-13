51ef2ba8b2173234d8904924aaedcebc2b923193
Erneststylz <erneststylz@gmail.com>
2022-10-13 09:20:55 -0700

    4. Hello, puts Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
 /**
  * Main - Entry point
  *
- * Retuen: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
